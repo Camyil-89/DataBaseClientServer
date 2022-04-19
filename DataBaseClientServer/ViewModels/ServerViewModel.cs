7ee0e477db2f8891ad734d0b7444eb3d8de7755f
Camyil-89 <zhukovkd@yandex.ru>
2022-04-19 14:10:28 +0300

    созданны базовыен классы и файлы

---
+++ DataBaseClientServer/ViewModels/ServerViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using DataBaseClientServer;
+using DataBaseClientServer.Base.Command;
+
+namespace DataBaseClientServer.ViewModels
+{
+	class ServerViewModel: Base.ViewModel.BaseViewModel
+	{
+		#region Commands
+		#region LoadServerCOmmand
+		public ServerViewModel()
+		{
+			#region Commands
+			LoadServerCommand = new LambdaCommand(OnLoadServerCommand, CanLoadServerCommand);
+			#endregion
+
+			Console.WriteLine("Start server");
+		}
+		public ICommand LoadServerCommand { get; set; }
+		public bool CanLoadServerCommand(object e) => true;
+		public void OnLoadServerCommand(object e)
+		{
+			
+		}
+		#endregion
+		#endregion
+	}
+}
