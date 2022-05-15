2396424c91d8f6cf71be65d14c26ed6a43a5aea6
Camyil-89 <zhukovkd@yandex.ru>
2022-05-15 14:31:24 +0300

    добавлена библиотека ExplorerDialog

---
+++ ExplorerDialog/FilePicker.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static ExplorerDialog.FolderPicker;
+
+namespace ExplorerDialog
+{
+	public class FilePicker
+	{
+		public static string ShowDialog(bool Multiselect, string Spliter)
+		{
+			OpenFileDialog fileDialog = new OpenFileDialog();
+			fileDialog.Multiselect = Multiselect;
+
+			fileDialog.ShowDialog();
+
+			if (Multiselect)
+			{
+				return string.Join(Spliter, fileDialog.FileNames);
+			}
+			else
+			{
+				return fileDialog.FileName;
+			}
+		}
+		public static string ShowDialog(bool Multiselect)
+		{
+			OpenFileDialog fileDialog = new OpenFileDialog();
+			fileDialog.Multiselect = Multiselect;
+
+			fileDialog.ShowDialog(new WindowWrapper(IntPtr.Zero));
+
+			if (Multiselect)
+			{
+				return string.Join(",", fileDialog.FileNames);
+			}
+			else
+			{
+				return fileDialog.FileName;
+			}
+		}
+	}
+}
